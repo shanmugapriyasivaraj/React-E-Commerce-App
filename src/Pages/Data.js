26d724d25fb3dfe2042ee220adda55ec08fb6539
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-05-09 05:42:51 +0530

    Create Slider

---
+++ src/Pages/Data.js
@@ -0,0 +1,23 @@
+export const SliderItems = [
+  {
+    id: 1,
+    img: "https://media.istockphoto.com/photos/smiling-girl-in-trendy-clothes-posing-at-studio-picture-id1033090858?k=20&m=1033090858&s=612x612&w=0&h=LIH4YdQiZeTBBcIBP7zmIDvOXPx8ipEsOIkdwkNoBmg=",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/cXFnLLV/3.png",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
