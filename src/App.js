2b377460ed9343fe6c3fb37d06416337a6e5f452
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-05-09 11:42:39 +0530

    Register & Login Page added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Product from "./Pages/Product";
 import Home from "./Pages/Home";
 import ProductList from "./Pages/ProductList";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
 
 function App() {
-  return <Product />;
+  return <Login />;
 }
 
 export default App;
